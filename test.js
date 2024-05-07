c97053c645b643649ff55b6279e084edf2f8c3df
aflynn0309 <107883034+aflynn0309@users.noreply.github.com>
2024-05-06 20:39:45 -0400

    first commit

---
+++ test.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const app = express();
+
+app.use((req, res, next) => {
+  const ip = req.socket.remoteAddress;
+  req.ipAddress = ip.includes('::ffff:') ? ip.slice(7) : ip;
+  next();
+});
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/public/index.html');
+});
+
+app.get('/grab', (req, res) => {
+  const ipAddress = req.ipAddress;
+  res.send(ipAddress);
+});
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
